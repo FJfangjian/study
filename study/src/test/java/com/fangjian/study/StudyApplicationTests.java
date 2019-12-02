7c8cb933b12b45ab245fc4ea32ada279af4bd117
fangjian <895469304@qq.com>
2019-12-02 22:17:42 +0800

    git修改测试

--- study/src/test/java/com/fangjian/study/StudyApplicationTests.java
+++ study/src/test/java/com/fangjian/study/StudyApplicationTests.java
@@ -8,32 +8,10 @@ class StudyApplicationTests {
 
 	@Test
 	void contextLoads() {
-		A a = new A();
 		ThreadLocal threadLocal = new ThreadLocal();
 		new Thread(()->{
 
 		});
 	}
 
-}
-class  A{
-	private String s;
-	private String sb;
-
-	public String getSb() {
-		return sb;
-	}
-
-	public void setSb(String sb) {
-		this.sb = sb;
-	}
-
-	public String getS() {
-
-		return s;
-	}
-
-	public void setS(String s) {
-		this.s = s;
-	}
 }
